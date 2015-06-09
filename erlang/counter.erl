9cbb248ffb74e381268aa4e9f7883bc3e4d395d0
Tim Pauls <tim.pauls.hh@gmail.com>
2015-06-09 17:02:49 +0200

    Clone.

--- erlang/counter.erl
+++ erlang/counter.erl
@@ -10,30 +10,44 @@ init(V, Ms, Func) ->
 	Gui = counterGui:start(V, Counter),
 	Counter!{guipid, Gui}.
 
+initClone(V, Counter) ->
+	Gui = counterGui:start(V, Counter),
+	Counter!{guipid, Gui}.
+
 preStopped(N, Ms) ->
 	receive
-		{guipid, Gui} -> stopped(N, Ms, Gui)
+		{guipid, Gui} -> stopped(N, Ms, [Gui])
 	end.
 
 preStarted(N, Ms) ->
 	receive
-		{guipid, Gui} -> started(N, Ms, Gui)
+		{guipid, Gui} -> started(N, Ms, [Gui])
 	end.
+
+closeGuis([Gui]) -> Gui!{gs, lol, destroy, lol, lol};
+closeGuis([Gui|T]) -> closeGuis([Gui]), closeGuis(T).
+
+messageGuis([Gui], N) -> Gui!{setValue, N};
+messageGuis([Gui|T], N) -> messageGuis([Gui], N), messageGuis(T, N).
 
 started(N, Ms, Gui) -> 
 	receive
 		stop -> stopped(N, Ms, Gui);
-		{close, Pid} -> exit(-1);
+		{close, Pid} -> closeGuis(Gui), exit(-1);
 		copy -> init(N, Ms, fun(X, Y) -> preStarted(X, Y) end), started(N, Ms, Gui);
+		clone -> initClone(N, self()), started(N, Ms, Gui);
+		{guipid, Pid} -> started(N, Ms, Gui ++ [Pid]);
 		_ -> started(N, Ms, Gui)
 
-		after Ms ->	Gui!{setValue, N}, started(N+1, Ms, Gui)
+		after Ms ->	messageGuis(Gui, N), started(N+1, Ms, Gui)
 	end.
 
 stopped(N, Ms, Gui) ->
 	receive
 		start -> started(N, Ms, Gui);
-		{close, Pid} -> exit(-1);
+		{close, Pid} -> closeGuis(Gui), exit(-1);
 		copy -> init(N, Ms, fun(X, Y) -> preStopped(X, Y) end), stopped(N, Ms, Gui);
+		clone -> initClone(N, self()), stopped(N, Ms, Gui);
+		{guipid, Pid} -> stopped(N, Ms, Gui ++ [Pid]);
 		_ -> stopped(N, Ms, Gui)
 	end.
