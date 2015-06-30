4c9cb8a7f9cc13b86fa5abc720d1e51db9fd79c1
inf101368 <inf101368@fh-wedel.de>
2015-06-30 15:14:50 +0200

    q.e.d even more!

--- haskell/chan.hs
+++ haskell/chan.hs
@@ -1,15 +1,29 @@
 module Main where
 import Control.Concurrent
 
-main = startTest 1
+main = do
+	startTestUnget 1
+	threadDelay 2000000
+	startTestEmpty 1
 
-startTest :: Int -> IO ()
-startTest n = do
+startTestEmpty :: Int -> IO ()
+startTestEmpty n = do
 	chan <- newChan
-	print "Start Thread 1"
+	print "Start Thread Reader"
 	forkIO (reader chan)
 	threadDelay 2000000
-	print "Start Thread 2"
+	print "Start Thread Empty"
+	forkIO (empty chan)
+	threadDelay 2000000
+	return ()
+
+startTestUnget :: Int -> IO ()
+startTestUnget n = do
+	chan <- newChan
+	print "Start Thread Reader"
+	forkIO (reader chan)
+	threadDelay 2000000
+	print "Start Thread Unget"
 	forkIO (ungetter chan)
 	threadDelay 2000000
 	return ()
@@ -19,7 +33,7 @@ reader channel = do
 	print "start read"
 	result <- readChan channel
 	threadDelay 2000000
-	print $ "result" ++ show result
+	print $ "result " ++ show result
 
 
 ungetter :: Chan Int -> IO ()
@@ -28,3 +42,10 @@ ungetter channel = do
 	unGetChan channel 1337
 	threadDelay 2000000
 	print "end unget"
+
+empty :: Chan Int -> IO ()
+empty channel = do
+	print "start isEmpty"
+	result <- isEmptyChan channel
+	threadDelay 2000000
+	print $ "result " ++ show result
