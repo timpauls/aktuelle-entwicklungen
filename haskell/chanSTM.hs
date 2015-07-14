5177c40655fd2c22195fbee6a9421356c081d821
Tim Pauls <tim.pauls.hh@gmail.com>
2015-07-14 13:35:10 +0200

    Implemented chan functions from assigment 5b

--- haskell/chanSTM.hs
+++ haskell/chanSTM.hs
@@ -29,6 +29,32 @@ readChan ch = do
       OwnMVar.putMVar ch ys
       return y
 
+isEmpty :: ChanSTM a -> STM Bool
+isEmpty ch = do
+  xs <- OwnMVar.tryTakeMVar ch
+  case xs of
+    (Just v) -> do
+      OwnMVar.putMVar ch v
+      return False
+    Nothing ->
+      return True
+
+ungetChan :: ChanSTM a -> a -> STM ()
+ungetChan ch x = do
+  xs <- OwnMVar.tryTakeMVar ch
+  case xs of
+    (Just v) ->
+      OwnMVar.putMVar ch ([x] ++ v)
+    Nothing ->
+      OwnMVar.putMVar ch [x]
+
+writeListToChan :: ChanSTM a -> [a] -> STM ()
+writeListToChan ch (a:[]) = ChanSTM.writeChan ch a
+writeListToChan ch (a:as) = do
+  ChanSTM.writeChan ch a
+  ChanSTM.writeListToChan ch as
+
+
 
 reader :: Show a => ChanSTM a -> IO ()
 reader ch = do
@@ -45,7 +71,24 @@ writer ch value = do
 main = do
   chan <- atomically ChanSTM.newChan
 
-  forkIO (writer chan 7)
-  forkIO (writer chan 13)
+  --Test Aufgabe 5a
+  --forkIO (writer chan 7)
+  --forkIO (writer chan 13)
+  --reader chan
 
-  reader chan
+  --Test Aufgabe 5b isEmpty
+  --atomically (ChanSTM.writeChan chan 5)
+  --forkIO (reader chan)
+  --threadDelay 500000
+  --empty <- (atomically (ChanSTM.isEmpty chan))
+  --print empty
+
+  -- Test unget
+  --atomically (ChanSTM.writeChan chan 5)
+  --forkIO (reader chan)
+  --threadDelay 500000
+  --atomically (ChanSTM.ungetChan chan 47)
+
+  -- Test writeListToChan
+  atomically (ChanSTM.writeListToChan chan [0..20000])
+  forkIO (reader chan)
