3593ade5017a86581c57b9788f6dbd4b9ec930cf
Mervyn McCreight <mervyn.mccreight@gmail.com>
2015-09-24 00:12:27 +0200

    fix old stm usage

--- go/stm/account.go
+++ go/stm/account.go
@@ -16,25 +16,25 @@ func NewAccount(amount int) *Account {
   }
 }
 
-func (a *Account) getBalance() (int, error) {
-  value, err := a.tvar.Get()
+func (a *Account) getBalance(state *stm.RWSet) (int, error) {
+  value, err := a.tvar.Get(state)
   return value.(int), err
 }
 
-func (a *Account) deposit(amount int) error {
-  current, err := a.getBalance()
-  a.tvar.Set(current + amount)
+func (a *Account) deposit(amount int, state *stm.RWSet) error {
+  current, err := a.getBalance(state)
+  a.tvar.Set(current + amount, state)
   return err
 }
 
-func (a *Account) transfer(to *Account, amount int) error {
-  err := a.deposit(-amount)
+func (a *Account) transfer(to *Account, amount int, state *stm.RWSet) error {
+  err := a.deposit(-amount, state)
 
   if (err != nil) {
     return err
   }
 
-  err = to.deposit(amount)
+  err = to.deposit(amount, state)
 
   return err
 }
@@ -43,18 +43,28 @@ func main() {
   k1 := NewAccount(100)
   k2 := NewAccount(200)
 
-  stm.Atomically(func () (stm.STMValue, error) {
-    err := k1.transfer(k2, 50)
+  atom1 := stm.Atomically()
+  atom1.SetAction(func () (stm.STMValue, error) {
+    err := k1.transfer(k2, 50, atom1.GetState())
+    return nil, err
+  })
+  atom1.Execute()
+
+  atom2 := stm.Atomically()
+  atom2.SetAction(func () (stm.STMValue, error) {
+    err := k2.transfer(k1, 10, atom2.GetState())
     return nil, err
   })
 
-  go stm.Atomically(func () (stm.STMValue, error) {
-    err := k2.transfer(k1, 10)
-    return nil, err
+  go atom2.Execute()
+
+  time.Sleep(1 * time.Second)
+
+  atom3 := stm.Atomically()
+  atom3.SetAction(func () (stm.STMValue, error) {
+    fmt.Println(k1.getBalance(atom3.GetState()))
+    fmt.Println(k2.getBalance(atom3.GetState()))
+    return nil, nil
   })
-
-  time.Sleep(2 * time.Second)
-
-  fmt.Println(k1.getBalance())
-  fmt.Println(k2.getBalance())
+  atom3.Execute()
 }
